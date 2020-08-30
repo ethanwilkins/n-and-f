7bffaef9a5f0b0c67c534495f260468b08d2b20f
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-08-30 14:13:54 -0400

    mobile_design_starting_to_come_along

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Head from 'next/head';
+import Navbar from '../components/Navbar';
 
 import '../index.scss';
+import styles from '../components/App.module.scss';
 
 export default function App() {
   return (
@@ -10,6 +12,27 @@ export default function App() {
         <title>N&F</title>
         <link rel="icon" href="/static/images/favicon.ico" />
       </Head>
+      
+      <Navbar />
+
+      <main>
+        <div className={styles.headline}>
+          We design and build user friendly digital experiences for your product and services.
+        </div>
+        <div className={styles.subheading}>
+          We do UX/UI design, product design, visual design, and website and web app development.
+        </div>
+        <div className={styles.getInTouchContainer}>
+          <img
+            src="/static/images/contactButton.svg"
+            className={styles.getInTouchButton}
+            alt="Get in touch!"
+          />
+        </div>
+      </main>
+      
+      <footer>
+      </footer>
     </div>
   )
 };
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,4 @@
 import React from 'react';
 import App from './_app';
-import HomePage from '../components/HomePage';
-
-export default function Home() {
-  return (
-    <div>
-      <App />
-      <HomePage />
-    </div>
-  )
-};
+const Home = () => <App />;
+export default Home;

ce675c6028d2bd75e015db0a67421c9fe88f3f40
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-08-30 18:54:49 -0400

    meta

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,20 @@ export default function App() {
       <Head>
         <title>N&F</title>
         <link rel="icon" href="/static/images/favicon.ico" />
+        
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, shrink-to-fit=no, user-scalable=no"
+        />
+        
+        <meta name="theme-color" content="#000000" />
+        <meta name="msapplication-navbutton-color" content="#000000" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#000000" />
+
+        <meta
+          name="description"
+          content="We do UX/UI design, product design, visual design, and website and web app development."
+        />
       </Head>
 
       <Navbar />
